99fbbbeb1ffb1980a51dc16d69ca2c323ac42dc3
hhefesto <hhefesto@rdataa.com>
2021-05-28 16:16:15 -0500

    now trying on github

--- flake.nix
+++ flake.nix
@@ -1,18 +1,21 @@
 {
   description = "Script ran by Github Actions to update Haddock documentation of telomare";
 
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
   inputs.flake-compat = {
     url = "github:edolstra/flake-compat";
     flake = false;
   };
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs, flake-compat }: {
+  outputs = { self, nixpkgs, flake-compat, haskellNix }: {
     defaultPackage.x86_64-linux =
       with import nixpkgs { system = "x86_64-linux"; };
       writeScriptBin "updateHaddockScript" ''
-        echo Hello1
-        echo hello2
+        cd telomare
+        nix develop
+        cabal haddock --haddock-hyperlink-source
+        rsync -a dist-newstyle/build/x86_64-linux/ghc-8.8.4/telomare-0.1.0.0/doc/html/telomare/ ../stand-in-language.github.io/docs/haddock/
       '';
   };
 }
